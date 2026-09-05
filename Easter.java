0b065b05730538dbdb15d6577969b4e1b5f1898f
Chinada18 <Parykilla1@gmail.com>
2026-09-04 23:41:49 -0400

    Enhance Easter date calculation and output details
    Updated the program to calculate Easter date for any year after 1583. Added detailed output for intermediate calculations.

--- Easter.java
+++ Easter.java
@@ -1,5 +1,5 @@
-// Your name
-// This program will calculate the date easter falls on given a year after 1583.
+// Killa Pary Mamani  09/03
+// This program will calculate the date easter falls on 2026.
 import java.util.*;
 
 public class Easter{
@@ -8,8 +8,42 @@ public class Easter{
         Scanner scanner = new Scanner(System.in); //this allows you to read input from the user.
         int y = scanner.nextInt(); // reads the next number the user types. 
         int a = y % 19;
-        System.out.println("a = "+a);
-        scanner.close();
+        int b = y / 100;
+        int c = y % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int f = (b + 8) / 25;
+        int g = (b - f + 1) / 3;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int r = (32 + 2 * e + 2 * i - h - k) % 7;
+        int m = (a + 11 * h + 22 * r) / 451;
+        int n = (h + r - 7 * m + 114) / 31;
+        int p = (h + r - 7 * m + 114) % 31;
+
+        int month = n;
+        int day = p + 1;
         
+        System.out.println("a = "+a);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+        System.out.println("i = " + i);
+        System.out.println("k = " + k);
+        System.out.println("r = " + r);
+        System.out.println("m = " + m);
+        System.out.println("n = " + n);
+        System.out.println("p = " + p);
+
+        
+        scanner.close();
+
+        System.out.println("Easter in " + y + " falls on " + month + "/" + day);
     }
 }
